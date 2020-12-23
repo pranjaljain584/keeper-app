8b2be70b27a0ab79e000a29ac5746ee37d1a1954
pranjaljain584 <62601082+pranjaljain584@users.noreply.github.com>
2020-12-23 13:54:39 +0530

    styling using react dependencies

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,41 @@
 import React , {useState} from "react";
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom';
 
 function CreateArea(props) {
+
+  const [isClicked , setZoom] = useState(false) ;
+
+  function handleClick(){
+    setZoom(true) ;
+  }
+
   return (
     <div>
-      <form onSubmit={props.onAddition}>
-        <input onChange={props.onChanged} name="title" placeholder="Title" value={props.noteEle.title} />
-        <textarea onChange={props.onChanged} name="content" placeholder="Take a note..." rows="3" value={props.noteEle.content} />
-        <button type="submit" >Add</button>
+      <form onSubmit={props.onAddition} className="create-note">
+
+        { isClicked && <input 
+        onChange={props.onChanged} 
+        name="title" 
+        placeholder="Title" 
+        value={props.noteEle.title} 
+        />}
+
+        <textarea 
+        onClick={handleClick}
+        onChange={props.onChanged} 
+        name="content" 
+        placeholder="Take a note..." 
+        rows={isClicked ? "3" : "1"} 
+        value={props.noteEle.content} 
+        />
+
+        <Zoom in={isClicked ? true : false}>
+          <Fab type="submit" >
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
