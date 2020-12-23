3f5102674033efbd69b0ef8ca1040278f5cefb3d
pranjaljain584 <62601082+pranjaljain584@users.noreply.github.com>
2020-12-23 12:55:06 +0530

    add note functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,60 @@
-import React from "react" ;
+import React, { useState } from "react" ;
 import Header from './Header';
 import Footer from './Footer' ;
 import Note from './Note' ;
 import CreateArea from "./CreateArea";
 
 function App(){
+
+    const [note , setNote] = useState({
+        title: "",
+        content: "" 
+    }) ;
+
+    const [notesArr , setNotes ] = useState([]) ;
+
+    function handleChange(event){
+        const {name,value} = event.target ;
+
+        setNote( prevValue => {
+            return {
+                ...prevValue,
+                [name] : value
+            } ;
+        }) ;
+
+    }
+
+    function addNote(event){
+      event.preventDefault() ;
+      setNotes( prevNotes => {
+        return [...prevNotes , note]
+      });
+
+      setNote({
+        title:"",
+        content:""
+      }) ;
+      
+    }
+    // console.log("arr",notesArr) ;
+
     return (
         <div>
             <Header />
-
-            {/* {notes.map( (note) => {
-                return (
-                    <Note  
-                      key={notes.key} 
-                      title={note.title}
-                      content={note.content} 
-                    />
-                )
-            })} */}
-
-            <CreateArea />
-            <Note key={1} title="Note title" content="Note content" />
-
+            <CreateArea 
+            onChanged = {handleChange}
+            onAddition = {addNote}
+            noteEle = {note}
+            />
+            {notesArr.map( (iNote , index) => (
+                <Note 
+                key={index} 
+                id={index}
+                title={iNote.title}
+                content={iNote.content}
+                />
+            ))}
             <Footer />
         </div>
     )
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,46 @@
-import React from "react";
+import React , {useState} from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+    
+    // const [note , setNote] = useState({
+    //     title: "",
+    //     content: "" 
+    // }) ;
+
+    // const [notesArr , setNotes ] = useState([]) ;
+
+    // function handleChange(event){
+    //     const {name,value} = event.target ;
+
+    //     setNote( prevValue => {
+    //         return {
+    //             ...prevValue,
+    //             [name] : value
+    //         } ;
+    //     }) ;
+
+    // }
+
+    // function addNote(event){
+    //   event.preventDefault() ;
+    //   setNotes( prevNotes => {
+    //     return [...prevNotes , note]
+    //   });
+
+    //   setNote({
+    //     title:"",
+    //     content:""
+    //   }) ;
+      
+    // }
+    // console.log("arr",notesArr) ;
+
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+      <form onSubmit={props.onAddition}>
+        <input onChange={props.onChanged} name="title" placeholder="Title" value={props.noteEle.title} />
+        <textarea onChange={props.onChanged} name="content" placeholder="Take a note..." rows="3" value={props.noteEle.content} />
+        <button type="submit" >Add</button>
       </form>
     </div>
   );
